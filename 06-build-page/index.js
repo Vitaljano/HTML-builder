117dfdd561e9b68b6db981bf6c723b7fb0bf94d4
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-05-23 18:16:14 +0300

    ref: refactor logic

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,76 +1,97 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fsi = require("fs");
+const fs = require("fs").promises;
 
+const DIST_FOLDER = "project-dist";
+const DEST_PATH = path.join(__dirname, DIST_FOLDER);
 const SRC_PATH = path.join(__dirname);
-const DEST_PATH = path.join(__dirname, 'project-dist');
 
-createFolder(path.join(__dirname, 'project-dist'));
-copyFolder(path.join(SRC_PATH, 'assets'), path.join(DEST_PATH, 'assets'));
-index();
-style();
+const assetsSrcPath = path.join(SRC_PATH, "assets");
+const assetsDestPath = path.join(DEST_PATH, "assets");
+const styleDestPath = path.join(DEST_PATH, "style.css");
+const styleSrcPath = path.join(SRC_PATH, "styles");
+const componentPath = path.join(__dirname, "components");
+const indexPath = path.join(DEST_PATH, "index.html");
+
+removeDist().then(() => {
+    createFolder(DEST_PATH);
+    copyAssetFolder(assetsSrcPath, assetsDestPath);
+    index();
+    style();
+});
+
+function removeDist() {
+    return fs.rm(DEST_PATH, { recursive: true, force: true });
+}
 async function createFolder(folderPath) {
-  await fs.promises.mkdir(folderPath, { recursive: true });
+    await fs.mkdir(folderPath, { recursive: true });
 }
 
-async function copyFolder(srcPath, destPath) {
-  const dir = await fs.promises.readdir(srcPath, { withFileTypes: true });
-  let currentPath = '';
-  // createFolder(destPath);
+async function copyAssetFolder(srcPath, destPath) {
+    const dir = await fs.readdir(srcPath, { withFileTypes: true });
+    let currentPath = "";
 
-  for (const element of dir) {
-    currentPath = path.join(srcPath, element.name);
+    for (const element of dir) {
+        currentPath = path.join(srcPath, element.name);
 
-    if (element.isDirectory()) {
-      await createFolder(path.join(destPath, element.name));
-      await copyFolder(currentPath, path.join(destPath, element.name));
-      // console.log(currentPath);
-    } else {
-      await fs.promises.copyFile(currentPath, path.join(destPath, element.name));
+        if (element.isDirectory()) {
+            await createFolder(path.join(destPath, element.name));
+            await copyAssetFolder(
+                currentPath,
+                path.join(destPath, element.name)
+            );
+        } else {
+            await fs.copyFile(currentPath, path.join(destPath, element.name));
+        }
     }
-  }
 }
 
 async function readTemplate() {
-  try {
-    const data = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8');
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const data = await fs.readFile(
+            path.join(__dirname, "template.html"),
+            "utf-8"
+        );
+        return data;
+    } catch (err) {
+        console.log("Ups something wrong with Template", err);
+    }
 }
 
 async function index() {
-  try {
-    const componentPath = path.join(__dirname, 'components');
-    const writeStream = await fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
-    let html = await readTemplate();
-    // Replace html template
-    // console.log(componentPath);
-    // console.log(html.replace('{{header}}', 'WTF'));
+    try {
+        let html = await readTemplate();
 
-    const components = await fs.promises.readdir(componentPath, { withFileTypes: true });
-    for (const component of components) {
-      const tmp = await fs.promises.readFile(path.join(componentPath, component.name));
-      const tmpName = path.basename(component.name, path.extname(component.name));
+        const components = await fs.readdir(componentPath, {
+            withFileTypes: true,
+        });
+        for (const component of components) {
+            const tmp = await fs.readFile(
+                path.join(componentPath, component.name)
+            );
+            const tmpExt = path.extname(component.name);
+            const tmpName = path.basename(component.name, tmpExt);
 
-      html = html.replace(`{{${tmpName}}}`, tmp);
+            html = html.replace(`{{${tmpName}}}`, tmp);
+        }
+
+        fs.writeFile(indexPath, html);
+    } catch (err) {
+        console.log("Ups something wrong Index html", err);
     }
-    writeStream.write(html);
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 async function style() {
-  const STYLE_PATH = path.join(__dirname, 'styles');
-  const STYLE_DIST_PATH = path.join(__dirname, 'project-dist', 'style.css');
-  let result = '';
-  const dir = await fs.promises.readdir(STYLE_PATH, { withFileTypes: true });
+    try {
+        let result = "";
+        const dir = await fs.readdir(styleSrcPath, { withFileTypes: true });
 
-  for (const file of dir) {
-    result += await fs.promises.readFile(path.join(STYLE_PATH, file.name));
-  }
+        for (const file of dir) {
+            result += await fs.readFile(path.join(styleSrcPath, file.name));
+        }
 
-  const writeStream = await fs.createWriteStream(STYLE_DIST_PATH);
-  writeStream.write(result);
+        fs.writeFile(styleDestPath, result);
+    } catch (err) {
+        console.log("Ups. something is wrong with Styles... ", err);
+    }
 }
