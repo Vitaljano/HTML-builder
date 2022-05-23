80c8d24220d598552ce577d010a34e352a20ba2c
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-05-23 18:28:25 +0300

    feat: done 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcPath = path.join(__dirname, 'files');
+const destPath = path.join(__dirname, 'file-copy');
+
+try {
+  readFolder(srcPath, destPath);
+} catch (error) {
+  console.log('Ups something is wrong', error);
+}
+
+async function createDir(destPath) {
+  await fs.promises.mkdir(destPath, { recursive: true });
+}
+
+async function copyFile(srcPath, destPath) {
+  fs.promises.copyFile(srcPath, destPath);
+}
+
+async function readFolder(srcPath, destPath) {
+  const folderFiles = await fs.promises.readdir(srcPath, { withFileTypes: true });
+  await createDir(destPath);
+  for (const file of folderFiles) {
+    copyFile(path.join(srcPath, file.name), path.join(destPath, file.name));
+  }
+}
